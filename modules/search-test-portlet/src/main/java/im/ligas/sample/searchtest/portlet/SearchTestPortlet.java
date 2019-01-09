96864be834582af4f69f377e0f3c92d803ff7b60
Miroslav Ligas <miroslav@ligas.im>
2019-01-09 23:51:27 +0000

    ddm field encoding sample

--- modules/search-test-portlet/src/main/java/im/ligas/sample/searchtest/portlet/SearchTestPortlet.java
+++ modules/search-test-portlet/src/main/java/im/ligas/sample/searchtest/portlet/SearchTestPortlet.java
@@ -1,5 +1,6 @@
 package im.ligas.sample.searchtest.portlet;
 
+import com.liferay.dynamic.data.mapping.util.DDMIndexer;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
@@ -9,6 +10,7 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import im.ligas.sample.searchtest.constants.SearchTestPortletKeys;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
@@ -38,6 +40,8 @@ public class SearchTestPortlet extends MVCPortlet {
 
     private static org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(SearchTestPortlet.class);
 
+    @Reference
+    private DDMIndexer ddmIndexer;
 
     @Override
     public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
@@ -76,6 +80,11 @@ public class SearchTestPortlet extends MVCPortlet {
             e.printStackTrace();
         }
 
+        //manually set values for testing
+        long structureId = 918493;
+        String searchTerm = "aaaaaaaaaa";
+        String structureField = "Text4n8h";
+        String categoryName = "zxcv";
 
         try {
             ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
@@ -97,7 +106,9 @@ public class SearchTestPortlet extends MVCPortlet {
             BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create(searchContext);
             booleanQuery.addRequiredTerm(Field.ENTRY_CLASS_NAME, JournalArticle.class.getName());
             booleanQuery.addRequiredTerm(Field.SCOPE_GROUP_ID, String.valueOf(themeDisplay.getScopeGroupId()));
-            booleanQuery.addRequiredTerm(Field.ASSET_CATEGORY_TITLES, "zxcv");
+            booleanQuery.addRequiredTerm(Field.ASSET_CATEGORY_TITLES, categoryName);
+            String fieldName = ddmIndexer.encodeName(structureId, structureField, themeDisplay.getLocale());
+            booleanQuery.addRequiredTerm(fieldName, searchTerm);
             Hits search = IndexSearcherHelperUtil.search(searchContext, booleanQuery);
 
             List<String> data = new ArrayList<>();
